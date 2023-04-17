9ccbaf06681cbc1927de1c7231813f763e107f63
João Vitor Ramos <joaovitor.ivd@gmail.com>
2023-04-17 20:26:12 +0000

    Beguinning of the project, missing who are you and others

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-for now, its nothing
+import os
+
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+
+# Initializes the app
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/aquele-dia")
+def thatDay():
+    return render_template("thatDay.html")
